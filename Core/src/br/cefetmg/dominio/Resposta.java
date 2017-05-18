47f8e14012a9c4f1a6dc59f664b40defdecf3699
Gabrielvgx <gabrielvgx@gmail.com>
2017-05-18 12:06:25 -0300

    Apaguei comentarios

--- Core/src/br/cefetmg/dominio/Resposta.java
+++ Core/src/br/cefetmg/dominio/Resposta.java
@@ -1,33 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.cefetmg.dominio;
 
 import java.util.Map;
 
-/**
- *
- * @author jaops
- */
 public class Resposta {
 
-   //  Questao questao;
-   // private Map<String, Boolean> alternativa;
-    //commit
     private int contRespostaCerta;
     private int contRespostaErrada;
-    private int variavel;
 
     public Resposta(Questao questao) {
         questao.getAlternativas();
     }
 
-/*    public Resposta(String textoAlternativa) {
-        alternativa.put(textoAlternativa, true);
-    }
-*/
     public boolean validacaoResposta(Questao questao) {
         for (Map.Entry<String, Boolean> entry : questao.getAlternativas().entrySet()) {
             String key = entry.getKey();
