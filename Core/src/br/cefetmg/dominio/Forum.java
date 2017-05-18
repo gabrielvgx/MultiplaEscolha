0c7b5255f8007e336a44e10aebeccf7f7b959c84
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-18 12:06:37 -0300

    Atualização da classe Forum
    Apaguei o comentário e a variável teste

--- Core/src/br/cefetmg/dominio/Forum.java
+++ Core/src/br/cefetmg/dominio/Forum.java
@@ -1,10 +1,10 @@
 package br.cefetmg.dominio;
 
-public class Forum {//Comentario
+public class Forum {
     private static long idComentario;
     private String comentario;
     private static long like;
-    private int teste;
+ 
     public Forum() {}
 
     public Forum(String comentario) {
