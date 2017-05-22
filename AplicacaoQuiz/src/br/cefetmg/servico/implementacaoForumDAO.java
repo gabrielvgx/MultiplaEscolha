1fe1edcfcea7636b6e4beb445ce5b64971f2ec37
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-22 12:54:11 -0300

    Atualização 22/05

--- AplicacaoQuiz/src/br/cefetmg/servico/implementacaoForumDAO.java
+++ AplicacaoQuiz/src/br/cefetmg/servico/implementacaoForumDAO.java
@@ -1,37 +1,56 @@
 package br.cefetmg.servico;
 
 import br.cefetmg.DAO.ForumDAO;
+import br.cefetmg.exception.ExcecaoNegocio;
+import br.cefetmg.exception.ExcecaoPersistencia;
 
-public class implementacaoForumDAO implements ForumDAO {
+public class implementacaoForumDAO implements PersistenciaForumDAO {
 
     private final ForumDAO forumDAO;
-    
+
     public implementacaoForumDAO(ForumDAO forumDAO) {
         this.forumDAO = forumDAO;
     }
-    
+
     @Override
-    public void inserir(long idQuestao, String comentario) {
+    public void inserirComentario(long idQuestao, String comentario) throws ExcecaoNegocio, ExcecaoPersistencia {
+
+        //Como comparar idQuestao com null para lançar a exceção?
+        
+        if (comentario == null) {
+            throw new ExcecaoNegocio("Nenhum comentário foi inserido.");
+        }
+
         forumDAO.inserir(idQuestao, comentario);
     }
-    
+
     @Override
-    public boolean editar(long idQuestao, long idComentario, String novoComentario) {
+    public boolean editarComentario(long idQuestao, long idComentario, String novoComentario) throws ExcecaoNegocio, ExcecaoPersistencia {
+
+        if (novoComentario == null) {
+            throw new ExcecaoNegocio("Nenhum novo comentário foi inserido.");
+        }
+
         return forumDAO.editar(idQuestao, idComentario, novoComentario);
     }
-    
+
     @Override
-    public void excluir(long idQuestao, long idComentario) {
+    public void excluirComentario(long idQuestao, long idComentario) throws ExcecaoPersistencia {
         forumDAO.excluir(idQuestao, idComentario);
     }
-    
+
     @Override
-    public void curtir(long idQuestao, long idComentario) {
+    public void curtirComentario(long idQuestao, long idComentario) throws ExcecaoNegocio, ExcecaoPersistencia {
         forumDAO.curtir(idQuestao, idComentario);
     }
-    
+
     @Override
-    public String pesquisar(long idQuestao, String palavraChave) {
+    public String pesquisarComentario(long idQuestao, String palavraChave) throws ExcecaoPersistencia {
+
+        if (palavraChave == null) {
+            throw new ExcecaoPersistencia("A palavra-chave não pode ser nula");
+        }
+
         return forumDAO.pesquisar(idQuestao, palavraChave);
-    }   
+    }
 }
