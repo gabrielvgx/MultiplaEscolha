047b8fdb83fdbd0665c257849c187ee925607bd4
Gabrielvgx <gabrielvgx@gmail.com>
2017-05-22 16:16:10 -0300

    Interface não utilizada
    A classe resposta passou a ser um atributo

--- AplicacaoQuiz/src/br/cefetmg/DAO/RespostaDAO.java
+++
@@ -1,8 +0,0 @@
-package br.cefetmg.DAO;
-
-import br.cefetmg.dominio.Resposta;
-
-public interface RespostaDAO {
-    public void gravarResposta (Resposta resposta);
-    public Resposta excluirResposta (Resposta resposta);  
-}
