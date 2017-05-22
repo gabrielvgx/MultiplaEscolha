21e117cfa233a173dd6d9e7754d93e53b3c075dc
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-22 12:53:18 -0300

    Atualização ForumDAO

--- AplicacaoQuiz/src/br/cefetmg/DAO/ForumDAO.java
+++ AplicacaoQuiz/src/br/cefetmg/DAO/ForumDAO.java
@@ -1,9 +1,11 @@
 package br.cefetmg.DAO;
 
+import br.cefetmg.exception.ExcecaoPersistencia;
+
 public interface ForumDAO {
-    public void inserir(long idQuestao, String comentario);
-    public boolean editar(long idQuestao, long idComentario, String novoComentario);
-    public void excluir(long idQuestao, long idComentario);
-    public void curtir(long idQuestao, long idComentario);
-    public String pesquisar(long idQuestao, String palavraChave);
+    public void inserir(long idQuestao, String comentario) throws ExcecaoPersistencia;
+    public boolean editar(long idQuestao, long idComentario, String novoComentario) throws ExcecaoPersistencia;
+    public void excluir(long idQuestao, long idComentario) throws ExcecaoPersistencia;
+    public void curtir(long idQuestao, long idComentario) throws ExcecaoPersistencia;
+    public String pesquisar(long idQuestao, String palavraChave) throws ExcecaoPersistencia;
 }
