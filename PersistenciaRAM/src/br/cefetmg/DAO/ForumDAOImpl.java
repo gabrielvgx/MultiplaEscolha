fe11e51887319cc4a41796ffc1019765ecb865a4
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-22 12:57:02 -0300

    Atualização 22/05

--- PersistenciaRAM/src/br/cefetmg/DAO/ForumDAOImpl.java
+++ PersistenciaRAM/src/br/cefetmg/DAO/ForumDAOImpl.java
@@ -1,7 +1,9 @@
 package br.cefetmg.DAO;
 
 import br.cefetmg.dominio.Forum;
+import br.cefetmg.exception.ExcecaoPersistencia;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +15,7 @@ public class ForumDAOImpl implements ForumDAO {
     private static List<Forum> forum = new ArrayList<>();
 
     private ForumDAOImpl() {
+        objeto = new LinkedHashMap<>();
     }
 
     public static ForumDAOImpl getInstance() {
@@ -23,51 +26,81 @@ public class ForumDAOImpl implements ForumDAO {
     }
 
     @Override
-    public void inserir(long idQuestao, String comentario) {
+    public void inserir(long idQuestao, String comentario) throws ExcecaoPersistencia {
+        /*
+        if (objeto.containsKey(idQuestao) == false) {
+            throw new ExcecaoPersistencia("O idQuestao não pode ser nulo");
+        }
+        */
+        if (comentario == null) {
+            throw new ExcecaoPersistencia("O comentario não pode ser nulo");
+        }
+        
         objeto.put(idQuestao, new Forum(comentario));
     }
 
     @Override
-    public boolean editar(long idQuestao, long idComentario, String novoComentario) {
-        if (objeto.containsKey(idQuestao)) {
-            if (objeto.containsKey(idComentario)) {
-                objeto.replace(idComentario, new Forum(novoComentario));
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
+    public boolean editar(long idQuestao, long idComentario, String novoComentario) throws ExcecaoPersistencia {
+        /*
+        if (objeto.containsKey(idQuestao) == false) {
+            throw new ExcecaoPersistencia("O idQuestao não pode ser nulo");
         }
+
+        if (objeto.containsKey(idComentario) == false) {
+            throw new ExcecaoPersistencia("O idComentario não pode ser nulo");
+        }
+        */
+        
+        if (novoComentario == null) {
+            throw new ExcecaoPersistencia("O novo comentario não pode ser nulo");
+        }
+
+        objeto.replace(idComentario, new Forum(novoComentario));
+
+        return true;
     }
 
     @Override
-    public void excluir(long idQuestao, long idComentario) {
-        if (objeto.containsKey(idQuestao)) {
-            if (objeto.containsKey(idComentario)) {
-                objeto.remove(idComentario);
-            }
+    public void excluir(long idQuestao, long idComentario) throws ExcecaoPersistencia {
+        if (objeto.containsKey(idQuestao) == false) {
+            throw new ExcecaoPersistencia("O idQuestao não pode ser nulo");
         }
+
+        if (objeto.containsKey(idComentario) == false) {
+            throw new ExcecaoPersistencia("O idComentario não pode ser nulo");
+        }
+
+        objeto.remove(idComentario);
     }
 
     @Override
-    public void curtir(long idQuestao, long idComentario) {
-        if (objeto.containsKey(idQuestao)) {
-            if (objeto.containsKey(idComentario)) {
-                objeto.get(idQuestao).incrementaLike();
-            }
+    public void curtir(long idQuestao, long idComentario) throws ExcecaoPersistencia {
+        if (objeto.containsKey(idQuestao) == false) {
+            throw new ExcecaoPersistencia("O idQuestao não pode ser nulo");
         }
+
+        if (objeto.containsKey(idComentario) == false) {
+            throw new ExcecaoPersistencia("O idComentario não pode ser nulo");
+        }
+
+        objeto.get(idQuestao).incrementaLike();
     }
 
     @Override
-    public String pesquisar(long idQuestao, String palavraChave) {
-        if (objeto.containsKey(idQuestao)) {
-            String comentario = objeto.get(idQuestao).getComentario();
-            if (comentario.contains(palavraChave)) {
-                return comentario;
-            } else {
-                return null;
-            }
+    public String pesquisar(long idQuestao, String palavraChave) throws ExcecaoPersistencia {
+        /*
+        if (objeto.containsKey(idQuestao) == false) {
+            throw new ExcecaoPersistencia("O idQuestao não pode ser nulo");
+        }
+
+        if (palavraChave == null) {
+            throw new ExcecaoPersistencia("A palavra-chave não pode ser nula");
+        }
+        */
+        
+        String comentario = objeto.get(idQuestao).getComentario();
+        if (comentario.contains(palavraChave)) {
+            return comentario;
         } else {
             return null;
         }
