2aa75139ee20299c2d7277e2599d9310ea4ea5cb
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-23 10:34:11 -0300

    Atualização 23/05

--- AplicacaoQuiz/src/br/cefetmg/servico/implementacaoDesempenhoDAO.java
+++ AplicacaoQuiz/src/br/cefetmg/servico/implementacaoDesempenhoDAO.java
@@ -1,24 +1,29 @@
 package br.cefetmg.servico;
 
 import br.cefetmg.DAO.DesempenhoDAO;
+import br.cefetmg.exception.ExcecaoNegocio;
+import br.cefetmg.exception.ExcecaoPersistencia;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class implementacaoDesempenhoDAO implements DesempenhoDAO {
+public class implementacaoDesempenhoDAO implements PersistenciaDesempenhoDAO {
 
-    private final DesempenhoDAO dao;
+    private final DesempenhoDAO desempenhoDAO;
 
     public implementacaoDesempenhoDAO(DesempenhoDAO dao) {
-        this.dao = dao;
+        this.desempenhoDAO = dao;
     }
 
     @Override
-    public void inserirDesempenho(int acertos, int tentativas) {
-        dao.inserirDesempenho(acertos, tentativas);
+    public void inserirDesempenho(int acertos, int tentativas) throws ExcecaoNegocio {
+        if((tentativas==0)&&(acertos>0)){
+            throw new ExcecaoNegocio ("Não é possível ter um número de acertos maior que zero se não forem realizadas tentativas");
+        }
+        desempenhoDAO.inserir(acertos, tentativas);
     }
 
     @Override
-    public Map<Long, ArrayList> consultarDesempenho() {
-        return dao.consultarDesempenho();  //informa a porcentagem de acertos
+    public Map<Long, ArrayList> consultarDesempenho() throws ExcecaoPersistencia {
+        return desempenhoDAO.consultar();  //informa a porcentagem de acertos.
     }
 }
