fd01c83f9cbed4e249a9302672b1317d8a6a507b
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-23 10:35:23 -0300

    Criação da interface PersistenciaDesempenhoDAO

---
+++ Core/src/br/cefetmg/servico/PersistenciaDesempenhoDAO.java
@@ -0,0 +1,11 @@
+package br.cefetmg.servico;
+
+import br.cefetmg.exception.ExcecaoNegocio;
+import br.cefetmg.exception.ExcecaoPersistencia;
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface PersistenciaDesempenhoDAO {
+    public void inserirDesempenho(int acertos, int tentativas) throws ExcecaoNegocio;
+    public Map <Long, ArrayList> consultarDesempenho() throws ExcecaoPersistencia;
+}
