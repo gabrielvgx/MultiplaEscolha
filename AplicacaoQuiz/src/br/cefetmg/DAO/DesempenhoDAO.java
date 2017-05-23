be35596550e7a0a51da65df3aad159a3b3b22135
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-23 10:33:25 -0300

    Atualização 23/05
    Desempenho - Dudu

--- AplicacaoQuiz/src/br/cefetmg/DAO/DesempenhoDAO.java
+++ AplicacaoQuiz/src/br/cefetmg/DAO/DesempenhoDAO.java
@@ -1,9 +1,11 @@
 package br.cefetmg.DAO;
 
+import br.cefetmg.exception.ExcecaoNegocio;
+import br.cefetmg.exception.ExcecaoPersistencia;
 import java.util.ArrayList;
 import java.util.Map;
 
 public interface DesempenhoDAO {
-    public void inserirDesempenho(int acertos, int tentativas);
-    public Map <Long, ArrayList> consultarDesempenho(); // retorna a id auto_increment e a relação de acertos, data de acesso e retorna os dados do ArrayList.
+    public void inserir(int acertos, int tentativas) throws ExcecaoNegocio;
+    public Map <Long, ArrayList> consultar() throws ExcecaoPersistencia; // retorna a id auto_increment e a relação de acertos, data de acesso e retorna os dados do ArrayList.
 }
