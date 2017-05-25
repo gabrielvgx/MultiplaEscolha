515d0410d361af60b55f6a68e465e59332aa24f3
Eduardo Moura <edumourap@gmail.com>
2017-05-25 11:06:17 -0300

    Alteração na assinatura do método consultar

--- AplicacaoQuiz/src/br/cefetmg/DAO/DesempenhoDAO.java
+++ AplicacaoQuiz/src/br/cefetmg/DAO/DesempenhoDAO.java
@@ -2,10 +2,8 @@ package br.cefetmg.DAO;
 
 import br.cefetmg.exception.ExcecaoNegocio;
 import br.cefetmg.exception.ExcecaoPersistencia;
-import java.util.ArrayList;
-import java.util.Map;
 
 public interface DesempenhoDAO {
     public void inserir(int acertos, int tentativas) throws ExcecaoNegocio;
-    public Map <Long, ArrayList> consultar() throws ExcecaoPersistencia; // retorna a id auto_increment e a relação de acertos, data de acesso e retorna os dados do ArrayList.
+    public double consultar(long idDesempenho) throws ExcecaoPersistencia; // retorna a id auto_increment e a relação de acertos, data de acesso e retorna os dados do ArrayList.
 }
