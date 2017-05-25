24b862627accecc7b32c485caa5730913ee5ebe8
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-25 11:57:32 -0300

    Alteração do método curtirComentario.

--- Core/src/br/cefetmg/servico/PersistenciaForumDAO.java
+++ Core/src/br/cefetmg/servico/PersistenciaForumDAO.java
@@ -7,6 +7,6 @@ public interface PersistenciaForumDAO {
     public void inserirComentario(long idQuestao, String comentario) throws ExcecaoNegocio, ExcecaoPersistencia;
     public boolean editarComentario(long idQuestao, long idComentario, String novoComentario) throws ExcecaoNegocio, ExcecaoPersistencia;
     public void excluirComentario(long idQuestao, long idComentario) throws ExcecaoPersistencia;
-    public void curtirComentario(long idQuestao, long idComentario) throws ExcecaoNegocio, ExcecaoPersistencia;
+    public void curtirComentario(long idQuestao, long idComentario) throws ExcecaoPersistencia;
     public String pesquisarComentario(long idQuestao, String palavraChave) throws ExcecaoPersistencia;
 }
