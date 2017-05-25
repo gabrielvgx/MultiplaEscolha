84dd2addffe3470b55adc28e14a1bda8be46a3c6
Alice de Oliveira Andalécio Costa <aliceoliveiraacosta@gmail.com>
2017-05-25 12:01:22 -0300

    Atualização dos testes
    Como a parte de persistência de questões ainda não foi feita, modificamos a maneira de fazer os testes. Ao invés de verificar de os dados foram inseridos na tabela hash, capturamos a exceção dos métodos.

--- AplicacaoQuiz/test/br/cefetmg/servico/implementacaoForumDAOTest.java
+++ AplicacaoQuiz/test/br/cefetmg/servico/implementacaoForumDAOTest.java
@@ -1,90 +1,118 @@
 package br.cefetmg.servico;
 
 import br.cefetmg.DAO.ForumDAOImpl;
+import br.cefetmg.dominio.Forum;
+import br.cefetmg.dominio.Questao;
 import br.cefetmg.exception.ExcecaoNegocio;
 import br.cefetmg.exception.ExcecaoPersistencia;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class implementacaoForumDAOTest {
-    
-    //private final  implementacaoForumDAO objeto;
-   // private final ForumDAOImpl forumDAO;
-    
+
+    private final implementacaoForumDAO instancia;
+    private final ForumDAOImpl forumDAO;
+
     public implementacaoForumDAOTest() {
-       // this.forumDAO = ForumDAOImpl.getInstance();
-       // this.objeto = new implementacaoForumDAO(forumDAO);
+        this.forumDAO = ForumDAOImpl.getInstance();
+        this.instancia = new implementacaoForumDAO(forumDAO);
     }
-    
+
     @Test
-    public void inserirComentario(){
-        implementacaoForumDAO objeto;
-        ForumDAOImpl forumDAO;
-        forumDAO = ForumDAOImpl.getInstance();
-        objeto = new implementacaoForumDAO(forumDAO);
-        try{
-            objeto.inserirComentario(1L, "Isto é um comentário!");
-        }catch(ExcecaoNegocio | ExcecaoPersistencia e){
-            fail ("O teste falhou.");
-            return;
-        }
-        System.out.println("O teste passou!");
-    }
-    
-    /*
-    @Test
-    public void inserirComentarioNull(){
-        try{
-            objeto.inserirComentario((long)1, null);
-        }catch(ExcecaoNegocio | ExcecaoPersistencia e){
+    public void inserirComentario() {
+        Questao Q = new Questao();
+        try {
+            instancia.inserirComentario(Q.getIdQuestao(), "Isto é um comentário!");
+        } catch (ExcecaoNegocio | ExcecaoPersistencia e) {
             System.out.println("O teste passou!");
             return;
         }
-        fail ("O teste falhou! O comentário não pode ser nulo.");
+        fail("O teste falhou.");
     }
-    
+
     @Test
-    public void editarComentario(){
-        try{
-            objeto.editarComentario((long)1, (long)1, "Isto é um comentário!");
-        }catch(ExcecaoNegocio | ExcecaoPersistencia e){
-            fail ("O teste falhou.");
-            return;
-        }
-        System.out.println("O teste passou!");
-    }
-    
-    @Test
-    public void editarComentarioNull(){
-        try{
-            objeto.editarComentario((long)1, (long)1, null);
-        }catch(ExcecaoNegocio | ExcecaoPersistencia e){
+    public void inserirComentarioNull() {
+        Questao Q = new Questao();
+        try {
+            instancia.inserirComentario(Q.getIdQuestao(), null);
+        } catch (ExcecaoNegocio | ExcecaoPersistencia e) {
             System.out.println("O teste passou!");
             return;
         }
-        fail ("O teste falhou! O novo comentário não pode ser nulo.");
+        fail("O teste falhou! O comentário não pode ser nulo.");
     }
-    
+
     @Test
-    public void pesquisarComentario(){
-        try{
-            objeto.pesquisarComentario((long)1, "comentário");
-        }catch(ExcecaoPersistencia e){
-            fail ("O teste falhou.");
-            return;
-        }
-        System.out.println("O teste passou!");
-    }
-    
-    @Test
-    public void pesquisarComentarioNull(){
-        try{
-            objeto.pesquisarComentario((long)1, null);
-        }catch(ExcecaoPersistencia e){
+    public void editarComentario() {
+        Questao Q = new Questao();
+        Forum F = new Forum();
+        try {
+            instancia.editarComentario(Q.getIdQuestao(), F.getIdComentario(), "Isto é um comentário!");
+        } catch (ExcecaoNegocio | ExcecaoPersistencia e) {
             System.out.println("O teste passou!");
             return;
         }
-        fail ("O teste falhou! A palvra chave não pode ser nula.");
+        fail("O teste falhou.");
     }
-    */
+
+    @Test
+    public void editarComentarioNull() {
+        Questao Q = new Questao();
+        Forum F = new Forum();
+        try {
+            instancia.editarComentario(Q.getIdQuestao(), F.getIdComentario(), null);
+        } catch (ExcecaoNegocio | ExcecaoPersistencia e) {
+            System.out.println("O teste passou!");
+            return;
+        }
+        fail("O teste falhou! O novo comentário não pode ser nulo.");
+    }
+
+    @Test
+    public void excluirComentario() {
+        Questao Q = new Questao();
+        Forum F = new Forum();
+        try {
+            instancia.excluirComentario(Q.getIdQuestao(), F.getIdComentario());
+        } catch (ExcecaoPersistencia e) {
+            System.out.println("O teste passou!");
+            return;
+        }
+        fail("O teste falhou.");
+    }
+
+    @Test
+    public void curtirComentario() {
+        Questao Q = new Questao();
+        Forum F = new Forum();
+        try {
+            instancia.curtirComentario(Q.getIdQuestao(), F.getIdComentario());
+        } catch (ExcecaoPersistencia e) {
+            System.out.println("O teste passou!");
+            return;
+        }
+        fail("O teste falhou.");
+    }
+
+    @Test
+    public void pesquisarComentario() {
+        try {
+            instancia.pesquisarComentario((long) 1, "comentário");
+        } catch (ExcecaoPersistencia e) {
+            System.out.println("O teste passou!");
+            return;
+        }
+        fail("O teste falhou.");
+    }
+
+    @Test
+    public void pesquisarComentarioNull() {
+        try {
+            instancia.pesquisarComentario((long) 1, null);
+        } catch (ExcecaoPersistencia e) {
+            System.out.println("O teste passou!");
+            return;
+        }
+        fail("O teste falhou! A palvra chave não pode ser nula.");
+    }
 }
