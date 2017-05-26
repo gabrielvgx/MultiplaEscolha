be2474b4240fdad1803869177e5e6420ee371b38
Jops99 <jaopss@outlook.com>
2017-05-25 23:03:03 -0300

    mudança construtor para publico
    o padrão não é mais Singleton.

--- PersistenciaRAM/src/br/cefetmg/DAO/QuestaoDAOImpl.java
+++ PersistenciaRAM/src/br/cefetmg/DAO/QuestaoDAOImpl.java
@@ -7,38 +7,39 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class QuestaoDAOImpl implements QuestaoDAO{
+public class QuestaoDAOImpl implements QuestaoDAO {
 
-    private static Map<Long,Questao> questaoDB = new LinkedHashMap<>();    
+    private static Map<Long, Questao> questaoDB = new LinkedHashMap<>();
     private Long contador; //numero de questoes no questaodb
-    
-    private QuestaoDAOImpl() {
-        contador=0L;
+
+    public QuestaoDAOImpl() {
+        contador = 0L;
     }
 
     @Override
-    synchronized public void inserir(Questao obj) throws ExcecaoPersistencia{
-        if(obj == null){
+    synchronized public void inserir(Questao obj) throws ExcecaoPersistencia {
+        if (obj == null) {
             throw new ExcecaoPersistencia("Questao não pode ser null");
         }
-        if (obj.getIdQuestao()!=null && questaoDB.containsKey(obj.getIdQuestao())){
+        if (obj.getIdQuestao() != null && questaoDB.containsKey(obj.getIdQuestao())) {
             throw new ExcecaoPersistencia("Chave duplicada");
         }
         Long idNovo = ++contador;
         obj.setIdQuestao(idNovo);
         questaoDB.put(idNovo, obj);
-                
+
     }
-    
-    
+
     @Override
     synchronized public void alterar(Questao obj) throws ExcecaoPersistencia {
 
-        if (obj == null)
-            throw new ExcecaoPersistencia("Entidade não pode ser nula.");              
-        if(obj.getIdQuestao() == null)
+        if (obj == null) {
+            throw new ExcecaoPersistencia("Entidade não pode ser nula.");
+        }
+        if (obj.getIdQuestao() == null) {
             throw new ExcecaoPersistencia("Questao sem id");
-        if(!questaoDB.containsKey(obj.getIdQuestao())){
+        }
+        if (!questaoDB.containsKey(obj.getIdQuestao())) {
             throw new ExcecaoPersistencia("Chave não cadastrada no banco de dados");
         }
         questaoDB.replace(obj.getIdQuestao(), obj);
@@ -47,30 +48,35 @@ public class QuestaoDAOImpl implements QuestaoDAO{
     @Override
     synchronized public Questao excluir(Long idQuestao) throws ExcecaoPersistencia {
 
-        if (idQuestao == null)
+        if (idQuestao == null) {
             throw new ExcecaoPersistencia("Chave da entidade não pode ser nulo.");
-        
-        if (!questaoDB.containsKey(idQuestao))
+        }
+
+        if (!questaoDB.containsKey(idQuestao)) {
             throw new ExcecaoPersistencia("Não existe entidade com a chave " + idQuestao + ".");
-        
+        }
+
         return questaoDB.remove(idQuestao);
     }
 
     @Override
     synchronized public Questao consultar(Long idQuestao) throws ExcecaoPersistencia {
-        
-        if (idQuestao == null)
+
+        if (idQuestao == null) {
             throw new ExcecaoPersistencia("Chave da entidade não pode ser nulo.");
-        
-        if (!questaoDB.containsKey(idQuestao))
+        }
+
+        if (!questaoDB.containsKey(idQuestao)) {
             throw new ExcecaoPersistencia("Não existe entidade com a chave " + idQuestao + ".");
-        
-        return questaoDB.get(idQuestao);        
-        
+        }
+
+        return questaoDB.get(idQuestao);
+
     }
+
     @Override
     public List<Questao> listar() throws ExcecaoPersistencia {
-        return new ArrayList<>(questaoDB.values());        
-    }    
+        return new ArrayList<>(questaoDB.values());
+    }
 
 }
